b899b1c8450c9908551ac58509af75b82d973eba
Jake Stewart <jakestewart95@outlook.com>
2019-06-08 18:27:59 +0100

    Contact From style and updates

--- contactform.php
+++ contactform.php
@@ -10,5 +10,5 @@ if (isset($_POST['submit'])) {
     $txt = "You have recieved an email from ".$name.".\n\n".$message;
 
     mail($mailTo, $subject, $txt, $headers);
-    header("Location: http://www.google.com/");
+    header("Location: contact.html?mailsent");
 }
